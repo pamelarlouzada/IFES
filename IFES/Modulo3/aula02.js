7aee7b035c26d473d10611d5496dab75b6eddb60
Pamela Rodrigues <pamela.rlouzada@gmail.com>
2022-12-12 23:22:41 -0300

    js semana!

--- IFES/Modulo3/aula02.js
+++ IFES/Modulo3/aula02.js
@@ -1,1 +1,11 @@
-console.log("Olá Mundo")
+// Demonstração de entrada de dados do usuário
+
+const prompt = require('prompt-sync')();
+
+nome = prompt("Entre com seu nome: ")
+idade = prompt("Entre com sua idade: ")
+endereco = prompt("Entre com seu endereço: ")
+
+console.log("Nome: ", nome)
+console.log("Idade: ", idade)
+console.log("Endereço: ", endereco)
