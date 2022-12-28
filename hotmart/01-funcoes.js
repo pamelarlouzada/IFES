561c50bf88919601f8cd0eb2030219dcb5fd53c5
pamela.louzada <pamela.louzada@sefaz.es.gov.br>
2022-12-28 17:00:37 -0300

    Aulas da hotmart

---
+++ hotmart/01-funcoes.js
@@ -0,0 +1,14 @@
+console.log("Aprendendo Funções")
+
+function media(n1, n2){
+    // var n1 = n1;
+    // var n2 = n2;
+    // var media = (n1 + n2)/2;
+    // return media;
+
+    return (n1+n2)/2
+}
+
+var result = media (10,6);
+console.log(result)
+
