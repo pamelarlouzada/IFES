6e6837742a31e8739dd912fd7daa63e3ac9e1689
pamela.louzada <pamela.louzada@sefaz.es.gov.br>
2023-01-02 17:02:35 -0300

    funcoes - hotmart

--- hotmart/01-funcoes.js
+++ hotmart/01-funcoes.js
@@ -9,6 +9,30 @@ function media(n1, n2){
     return (n1+n2)/2
 }
 
-var result = media (10,6);
-console.log(result)
+// formas de chamar a função:
 
+console.log(media) //imprime a função
+
+var m1 = media (1,1);
+console.log(m1) 
+
+var m2 = media;
+var m3 = m2(2,2)
+console.log(m3) 
+
+
+// função anônima
+console.log("Função anônima") 
+var media = function (n1,n2) { 
+    return (n1+n2)/2;
+}
+
+console.log(media(3,3))
+
+// Outra forma de criar função
+console.log("Outra forma de criar função") 
+var media = (n1,n2) => { 
+    return (n1+n2)/2;
+}
+
+console.log(media(4,4))
