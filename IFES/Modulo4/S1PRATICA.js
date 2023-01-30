6cfb43874418deaec9ea10915e089b4a2105d1bb
Pamela Rodrigues <pamela.rlouzada@gmail.com>
2023-01-29 22:09:48 -0300

    Inserindo a pratica

---
+++ IFES/Modulo4/S1PRATICA.js
@@ -0,0 +1,5 @@
+// Selecionar todos elementos da classe 'produto_preco'
+
+// Implementar um comando de repetição para analisar o valor 'innerText' de cada um dos elementos e somá-los (lembre-se de converter o valor para float)
+
+// Escrever no conteúdo da página o valor da soma
